c207b8c23dcec0a504c404bf107fe87b9343a325
Parth Shukla <31998380+ParthShuklaa@users.noreply.github.com>
2021-10-20 12:55:58 +0530

    Updated till Day 5/6th - 20th Wednesday 2021

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Looping_Construct
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your name");
+            string Name = Console.ReadLine();
+
+            Console.Write("How many times you want to Display Your name ");
+            int Count = Convert.ToInt32(Console.ReadLine());
+
+
+
+            for (int i = 0; i < Count; i++)
+            {
+                Console.WriteLine(Name);
+
+            }
+
+            
+
+            //Entry Controlled loop
+            //While()
+            //Exit Controlled loop
+            //Dowhile()
+        }
+    }
+}
